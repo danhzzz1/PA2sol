fb330113b75ae428ee206a34f0ec6dc513110dfd
danhzzz1 <dtnguyen@ucsd.edu>
2020-04-13 13:19:59 -0700

    change

--- BST.cpp
+++ BST.cpp
@@ -6,6 +6,8 @@
  */
 BST::BST() {
     /* YOUR CODE HERE */
+    root = NULL;
+    numElements = 0;
 }
 
 /**
@@ -13,6 +15,7 @@ BST::BST() {
  */
 BST::~BST() {
     /* YOUR CODE HERE */
+    clear();
 }
 
 /**
@@ -20,13 +23,26 @@ BST::~BST() {
  */
 unsigned int BST::size() const {
     /* YOUR CODE HERE */
+    return numElements;
 }
-
+void myHelper(BST::Node* node) {
+    // do something
+    if(node){    
+        if(node -> leftChild)
+            myHelper(node -> leftChild);
+        if(node -> rightChild)
+            myHelper(node -> rightChild);
+        delete node;
+    }
+}
 /**
  * Implement clear() correctly without memory leaks
  */
 void BST::clear() {
     /* YOUR CODE HERE */
+    myHelper(root);
+    root = NULL;
+    numElements = 0;
 }
 
 /**
@@ -34,6 +50,35 @@ void BST::clear() {
  */
 bool BST::insert(int element) {
     /* YOUR CODE HERE */
+    if(!root){
+        numElements++;
+        root = new Node(element);
+        return 1;
+    }
+    Node *temp = root;
+    while(temp -> data != element){
+        if(element < temp -> data){
+            if(temp ->leftChild == NULL){
+                temp -> leftChild = new Node(element);
+                numElements++;
+                return 1;
+            }
+            else{
+                temp = temp -> leftChild;
+            }
+        }
+        else if(element > temp -> data){
+            if(temp ->rightChild == NULL){
+                temp -> rightChild = new Node(element);
+                numElements++;
+                return 1;
+            }
+            else{
+                temp = temp -> rightChild;
+            }
+        }
+    }
+    return 0;
 }
 
 /**
@@ -41,6 +86,19 @@ bool BST::insert(int element) {
  */
 bool BST::find(const int & query) const {
     /* YOUR CODE HERE */
+    Node *temp = root;
+    while(query != temp -> data){
+        if(query < temp -> data){
+            temp = temp -> leftChild;
+        }
+        else if(query > temp -> data){
+            temp = temp -> rightChild;
+        }
+        if(!temp){
+            return false;
+        }
+    }
+    return true;
 }
 
 /**
@@ -48,6 +106,14 @@ bool BST::find(const int & query) const {
  */
 BST::Node* BST::getLeftMostNode() {
     /* YOUR CODE HERE */
+    Node *temp = root;
+    if(!temp){
+        return NULL;
+    }
+    while ((temp -> leftChild)){
+        temp = temp -> leftChild;
+    }
+    return temp;
 }
 
 /**
@@ -55,4 +121,24 @@ BST::Node* BST::getLeftMostNode() {
  */
 BST::Node* BST::Node::successor() {
     /* YOUR CODE HERE */
+    Node *temp;
+    if(rightChild != NULL){
+        temp = rightChild;
+        while(temp-> leftChild != NULL){
+            temp = temp -> leftChild;
+        }
+        return temp;
+    }
+    else{
+        temp = this;
+        while(temp -> parent != NULL){
+            if(temp == temp -> parent -> leftChild){
+                return temp -> parent;    
+            }
+            else{
+                temp = temp -> parent;
+            }
+        }
+        return NULL;
+    }
 }
